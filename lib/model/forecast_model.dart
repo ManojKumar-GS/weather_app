c8fe64cfd90f14d3468c138f6f6894c15d581bff
ManojKumar-GS <manojkumargs195@gmail.com>
2024-07-03 22:55:14 +0530

    forecast model modification

--- lib/model/forecast_model.dart
+++ lib/model/forecast_model.dart
@@ -1,16 +1,16 @@
 class Forecast {
-  final String cod;
-  final int message;
-  final int cnt;
-  final List<WeatherInfo> list;
-  final City city;
+  final String? cod;
+  final int? message;
+  final int? cnt;
+  final List<WeatherInfo>? list;
+  final City? city;
 
   Forecast({
-    required this.cod,
-    required this.message,
-    required this.cnt,
-    required this.list,
-    required this.city,
+    this.cod,
+    this.message,
+    this.cnt,
+    this.list,
+    this.city,
   });
 
   factory Forecast.fromJson(Map<String, dynamic> json) {
