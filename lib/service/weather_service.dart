3f19bac422e5336b29dcc3d9f3c15133821d7256
ManojKumar-GS <manojkumargs195@gmail.com>
2024-07-07 12:44:00 +0530

    forecast endpoint modification

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/model/forecast_model.dart';
 import 'package:weather_app/model/weather_model.dart';
 
 class WeatherService {
@@ -29,12 +30,17 @@ class WeatherService {
     }
   }
 
-  static getWeatherForecast() async {
-    var data = {'appid': dotenv.env["api_key"], 'units': 'metric'};
-    final response = await dio.get(weatherURL, queryParameters: data);
-    if (response.data['cod'] == 200) {
-      return WeatherModel.fromJson(response.data);
+  static getWeatherForecast(
+      {required double latitude, required double longitude}) async {
+    var data = {
+      'lat': latitude,
+      'lon': longitude,
+      'appid': dotenv.env["api_key"],
+      'units': 'metric'
+    };
+    final response = await dio.get(forecastURL, queryParameters: data);
+    if (response.data['cod'] == '200') {
+      return Forecast.fromJson(response.data);
     }
   }
 }
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
